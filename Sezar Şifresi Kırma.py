1f5009c7fe39a8cb21b5ed8ad0a2a426b66aa2e1
rexlier <122105608+rexlier@users.noreply.github.com>
2023-01-06 17:46:59 +0300

    Update Sezar Şifresi Kırma.py

--- Sezar Şifresi Kırma.py
+++ Sezar Şifresi Kırma.py
@@ -1,5 +1,7 @@
 print("Sezar Şifresi Kırma Programına Hoşgeldiniz.")
-sifresiz=input("Şifresi kırılacak metni giriniz:")
+sifresiz=input(""T3 Akademi; Türkiye Teknoloji Takımı Vakfı tarafından hayata geçirilen Teknofest, Deneyap, Bilim Türkiye, Deneyapkart, Keşif Kampüsü, Girişim Merkezi, T3 Podcast içeriklerinin yayınlandığı bir çevrimiçi öğrenme sistemidir. T3 Akademi’de ayrıca bilgi teknolojileri ve yazılım, dijital içerik ve medya kişisel gelişim, girişimcilik, eğitim bilimleri, dil öğrenimi, sosyal bilimler gibi birçok farklı kategoride eğitim içeriği de yer almaktadır."
+
+:")
 def sifrele(metin):
     sifrelimetin=""
     for harf in metin:
