ee7ee6c4ea3b4049659d30c1166e7e41a88b907d
rexlier <122105608+rexlier@users.noreply.github.com>
2023-01-06 17:47:44 +0300

    Update Sezar Şifresi Kırma.py

--- Sezar Şifresi Kırma.py
+++ Sezar Şifresi Kırma.py
@@ -1,9 +1,13 @@
 print("Sezar Şifresi Kırma Programına Hoşgeldiniz.")
-sifresiz=input("Şifresi kırılacak metni giriniz:")
+sifresiz=input(""T3 Akademi; Türkiye Teknoloji Takımı Vakfı tarafından hayata geçirilen Teknofest, Deneyap, Bilim Türkiye, Deneyapkart, Keşif Kampüsü, Girişim Merkezi, T3 Podcast içeriklerinin yayınlandığı bir çevrimiçi öğrenme sistemidir. T3 Akademi’de ayrıca bilgi teknolojileri ve yazılım, dijital içerik ve medya kişisel gelişim, girişimcilik, eğitim bilimleri, dil öğrenimi, sosyal bilimler gibi birçok farklı kategoride eğitim içeriği de yer almaktadır."
+
+:")
 def sifrele(metin):
-    sifrelimetin=""
+    sifrelimetin=""T3 Akademi; Türkiye Teknoloji Takımı Vakfı tarafından hayata geçirilen Teknofest, Deneyap, Bilim Türkiye, Deneyapkart, Keşif Kampüsü, Girişim Merkezi, T3 Podcast içeriklerinin yayınlandığı bir çevrimiçi öğrenme sistemidir. T3 Akademi’de ayrıca bilgi teknolojileri ve yazılım, dijital içerik ve medya kişisel gelişim, girişimcilik, eğitim bilimleri, dil öğrenimi, sosyal bilimler gibi birçok farklı kategoride eğitim içeriği de yer almaktadır."
+
+"
     for harf in metin:
-        asciikod=ord(harf)
+        asciikod=ord(29)
         asciikod=asciikod-3
         karakterkod=chr(asciikod)
         sifrelimetin=sifrelimetin+karakterkod
